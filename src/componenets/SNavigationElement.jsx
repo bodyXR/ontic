746b3b37c271258326a2ce246c9360f5353b4567
madoelmalah4 <152697670+madoelmalah4@users.noreply.github.com>
2024-07-09 04:22:58 +0300

    Making login and resister pages

---
+++ src/componenets/SNavigationElement.jsx
@@ -0,0 +1,16 @@
+import { styled } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+
+const SNavigationElement = styled(NavLink)(() => ({
+  fontSize: 30,
+  fontFamily:"Arial",
+  fontWeight: 'bold',
+  textTransform: "capitalize",
+  color: "#424242",
+  textDecoration: "none",
+  "&.activeelement": {
+    borderBottom: `2px solid #424242`,
+  },
+}));
+
+export default SNavigationElement;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
-import InputComponent from '../componenets/InputComponent'
-
+import React, { useEffect, useState } from "react";
+import InputComponent from "../componenets/InputComponent";
+import { Stack, Typography } from "@mui/material";
+import ButtonComponent from "../componenets/ButtonComponent";
+import SNavigationElement from "../componenets/SNavigationElement";
+import { lightGreen } from "@mui/material/colors";
 const Login = () => {
-  return (
-    <div>
-      <InputComponent placeholder={'enter email'} label={'email'}/>
-    </div>
-  )
-}
 
-export default Login
+  return (
+    <>
+      <Stack
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          bgcolor: "#FFFFFF",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Stack
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "2rem",
+          }}
+        >
+          <Typography
+            sx={{ fontSize: "40px", fontWeight: "bold", color: "#424242" }}
+          >
+            Login
+          </Typography>
+          <InputComponent label={"Enter your Email"} />
+          <InputComponent label={"Enter your Password"} />
+        </Stack>
+        <Typography
+          sx={{
+            color: "blue",
+            textDecoration: "underline",
+            marginLeft: "165px",
+            marginTop: "5px",
+            fontSize: "15px",
+            color: "#0066FF",
+          }}
+        >
+          forget password?
+        </Typography>
+        <Stack
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: "4rem",
+          }}
+        >
+          <ButtonComponent text={"Login"} />
+          <Typography
+            sx={{
+              color: "blue",
+              textDecoration: "underline",
+              fontSize: "15px",
+              color: "#0066FF",
+            }}
+          >
+            Don’t have an account?
+          </Typography>
+        </Stack>
+      </Stack>
+    </>
+  );
+};
+
+export default Login;
