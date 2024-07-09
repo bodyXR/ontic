ccfce306212f695cd98fadd8401bc11acc0cd361
abdullah mohamed <116273298+bodyXR@users.noreply.github.com>
2024-07-09 16:43:15 +0300

    add footer

--- src/componenets/Footer.jsx
+++ src/componenets/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <Stack
       sx={{
         background: "#1E1E1E",
-        gap: "15px",
+        gap: "10px",
         height: "60px",
         pt: "15px",
         flexDirection: "row",
@@ -20,31 +20,31 @@ const Footer = () => {
     >
       <Button sx={{ width: "30px", height: "30px" }}>
         <Link to="/home">
-        <img sx={{ width: "10px", objectFit: "cover" }} src={home} alt="" />
+          <img sx={{ width: "10px", objectFit: "cover" }} src={home} alt="" />
         </Link>
       </Button>
       <Button sx={{ width: "30px", height: "30px" }}>
         <Link to="/videos">
-        <img sx={{ width: "10px", objectFit: "cover" }} src={videos} alt="" />
+          <img sx={{ width: "10px", objectFit: "cover" }} src={videos} alt="" />
         </Link>
-
       </Button>
       <Button sx={{ width: "30px", height: "30px" }}>
         {/* <Link to="/"> */}
         <img sx={{ width: "10px", objectFit: "cover" }} src={add} alt="" />
         {/* </Link> */}
-
       </Button>
       <Button sx={{ width: "30px", height: "30px" }}>
         <Link to="/courses">
-        <img sx={{ width: "10px", objectFit: "cover" }} src={courses} alt="" />
+          <img
+            sx={{ width: "10px", objectFit: "cover" }}
+            src={courses}
+            alt=""
+          />
         </Link>
-
       </Button>
       <Button sx={{ width: "30px", height: "30px" }}>
         <img sx={{ width: "10px", objectFit: "cover" }} src={profile} alt="" />
-        <Link to="">
-        </Link>
+        <Link to=""></Link>
       </Button>
     </Stack>
   );
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import header from '../assets/header.png'
 import coursesPage from '../assets/coursesPage.png'
+import Footer from '../componenets/Footer'
 
 const Courses = () => {
   return (
     <div>
       <img src={header} alt="" />
       <img src={coursesPage} alt="" style={{marginTop:'30px'}} />
+      <Footer/>
     </div>
   )
 }
